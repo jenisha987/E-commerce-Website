d8d3900ec653d432f58a63f8b40bd7db8e7c852a
jenisha987 <jensastha987@gmail.com>
2024-09-01 20:58:56 +0545

    Navbar section

--- e-commerce/src/components/Navbar.jsx
+++ e-commerce/src/components/Navbar.jsx
@@ -1,37 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from "../assets/images/fashion.png";
+import { HiShoppingBag } from "react-icons/hi2";
+import { CiSearch } from "react-icons/ci";
+import { PiShoppingCartThin } from "react-icons/pi";
+import { MdMenu } from "react-icons/md";
 
 export default function Navbar() {
+
+    const [ open, setOpen ] = useState(false);
 
     const navlinks = [
         {
             id: 1,
-            text: "home"
+            title: "home"
         },
         {
             id: 2,
-            text: "menu"
+            title: "menu"
         },
         {
             id: 3,
-            text: "mobile-app"
+            title: "MobileApp"
         },
         {
             id: 4,
-            text: "contact-us"
+            title: "contactUs"
         },
     ]
 
     return (
-        <div className="flex justify-between items-center shadow-lg w-full h-20 ">
-            <img src={logo} alt="logo" className="md:w-40 h-40 w-32" />
-            <nav className="hidden md:flex">
-                {navlinks.map(({ id, text }) => (
-                    <li key={id} className='px-4 list-none cursor-pointer capitalize font-medium hover:scale-90 duration-200'>
-                        { text }
-                    </li>
-                ))}
+        <>
+            <nav>
+                <div className='container flex justify-between items-center py-8'>
+                    {/* Logo Section */}
+                    <div className='text-2xl flex items-center gap-2 font-bold uppercase'>
+                        <HiShoppingBag />
+                        <p>Fashion</p>
+                        <p className='text-secondary'>Wear</p>
+                    </div>
+                    {/* Menu Section */}
+                    <div className='hidden md:block'>
+                        <ul className='flex items-center gap-6 text-gray-600'>
+                            {
+                                navlinks.map(({ id, title }) => (
+                                    <li key={id} className='inline-block py-1 px-3 hover:text-primary font-semibold cursor-pointer capitalize'>
+                                        {title}
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                    {/* Icons Section */}
+                    <div className='flex items-center gap-4'>
+                        <button className='text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200'>
+                            <CiSearch />
+                        </button>
+                        <button className='text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200'>
+                            <PiShoppingCartThin />
+                        </button>
+                        <button className='hover:bg-primary text-primary font-semibold hover:text-white
+                        rounded-md border-2 border-primary px-6 py-2 duration-200 hidden md:block'>
+                            Login
+                        </button>
+                    </div>
+                    {/* Mobile Hamburger Menu Section */}
+                    <div className='md:hidden' onClick={() => setOpen(!open)}>
+                        <MdMenu className='text-4xl' />
+                    </div>
+                </div>
             </nav>
-        </div>
+        </>
     )
 }
