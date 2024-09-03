761e8ae63852eaef3aac6aea619b723cf03ba158
jenisha987 <jensastha987@gmail.com>
2024-09-03 17:08:25 +0545

    Layout component and Routes

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Layout from "./components/Layout";
+import Cart from "./pages/Cart";
+import PlaceOrder from "./pages/PlaceOrder";
+
+
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<Layout />} >
+        <Route index element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+    </Route>
+))
 
 export default function App() {
     return (
         <div className="overflow-x-hidden">
-            <Navbar />
+            <RouterProvider router={router} />
         </div>
     )
 }
