1e009777c389913e0e8692137004709715cb05b4
jenisha987 <jensastha987@gmail.com>
2024-09-04 19:51:42 +0545

    styling menu section

--- e-commerce/src/assets/assets.js
+++ e-commerce/src/assets/assets.js
@@ -25,34 +25,42 @@ export const navlinks = [
 
 export const menu_list = [
     {
+        id: 1,
         menu_name: "T-shirt",
         menu_image: t_shirt
     },
     {
+        id: 2,
         menu_name: "Shirt",
         menu_image: t_shirt
     },
     {
+        id: 3,
         menu_name: "Traditional Wear",
         menu_image: t_shirt
     },
     {
+        id: 4,
         menu_name: "Caps",
         menu_image: t_shirt
     },
     {
+        id: 5,
         menu_name: "Hand Bag",
         menu_image: t_shirt
     },
     {
+        id: 6,
         menu_name: "Heels",
         menu_image: t_shirt
     },
     {
+        id: 7,
         menu_name: "Mens Shoes",
         menu_image: t_shirt
     },
     {
+        id: 8,
         menu_name: "Shorts",
         menu_image: t_shirt
     },
--- e-commerce/src/components/ExploreMenu.jsx
+++ e-commerce/src/components/ExploreMenu.jsx
@@ -3,14 +3,14 @@ import { menu_list } from "../assets/assets";
 
 export default function ExploreMenu() {
     return (
-        <div className="" id="explore-menu">
-            <h1>Explore Our Latest Products</h1>
-            <div>
+        <div className="flex flex-col gap-5" id="explore-menu">
+            <h1 className="md:text-4xl text-2xl font-bold">Explore Our Latest Products</h1>
+            <div className="grid md:grid-cols-4 grid-cols-2 mx-auto gap-6 text-center">
                 {
                     menu_list.map(({ id, menu_image, menu_name }) => {
                         return (
                             <div key={id}>
-                                <img src={menu_image} alt="menu" />
+                                <img src={menu_image} alt="menu" className="h-60" />
                                 <p>{menu_name}</p>
                             </div>
                         )
