72f973cabe273628e587b3b9f07a5dfb2d7feea9
jenisha987 <jensastha987@gmail.com>
2024-09-04 19:28:44 +0545

    creating menu section

--- e-commerce/src/components/ExploreMenu.jsx
+++ e-commerce/src/components/ExploreMenu.jsx
@@ -5,6 +5,18 @@ export default function ExploreMenu() {
     return (
         <div className="" id="explore-menu">
             <h1>Explore Our Latest Products</h1>
+            <div>
+                {
+                    menu_list.map(({ id, menu_image, menu_name }) => {
+                        return (
+                            <div key={id}>
+                                <img src={menu_image} alt="menu" />
+                                <p>{menu_name}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
--- e-commerce/src/pages/Home.jsx
+++ e-commerce/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import ExploreMenu from "../components/ExploreMenu";
 
 export default function Home() {
     return (
         <div className="container">
             <Header />
+            <ExploreMenu />
         </div>
     )
 }
