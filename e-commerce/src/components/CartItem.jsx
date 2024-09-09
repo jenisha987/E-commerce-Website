02d41ef077ebbc4c69e1310928f3d3ec89a87dc6
jenisha987 <jensastha987@gmail.com>
2024-09-09 20:42:50 +0545

    adding oncrement & decrement functionality

--- e-commerce/src/components/CartItem.jsx
+++ e-commerce/src/components/CartItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import { MdDelete } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
-import { removeFromCart } from "../redux/slices/CartSlice";
+import { removeFromCart, incrementQty, decrementQty } from "../redux/slices/CartSlice";
 
 export default function CartItem({ id, name, img, price, qty }) {
 
@@ -10,7 +10,7 @@ export default function CartItem({ id, name, img, price, qty }) {
 
     return (
         <div className="flex gap-2 shadow-md rounded-lg p-2 mb-3">
-            <MdDelete onClick={() => dispatch(removeFromCart({id, img, name, price, qty}))} size={18} className="absolute right-7 text-gray-800 cursor-pointer" />
+            <MdDelete onClick={() => dispatch(removeFromCart({ id }))} size={18} className="absolute right-7 text-gray-800 cursor-pointer" />
             <img 
                 src={img}
                 alt="CartItem" 
@@ -21,10 +21,10 @@ export default function CartItem({ id, name, img, price, qty }) {
                 <div className="flex">
                     <span className="text-primary font-semibold">Rs.{price}</span>
                     <div className="flex justify-center items-center gap-2 absolute right-7">
-                        <AiOutlinePlus size={20} className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-primary 
+                        <AiOutlineMinus onClick={() => qty > 1 ? dispatch(decrementQty({ id })) : (qty = 0) } size={20} className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-primary 
                             hover:border-none rounded-full p-1 text-xl transition-all ease-linear cursor-pointer" />
                         <span>{qty}</span>
-                        <AiOutlineMinus size={20} className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-primary 
+                        <AiOutlinePlus onClick={() => qty >= 1 ? dispatch(incrementQty({ id })) : (qty = 0) } size={20} className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-primary 
                             hover:border-none rounded-full p-1 text-xl transition-all ease-linear cursor-pointer" />
                     </div>
                 </div>
