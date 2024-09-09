9b23e9db27261074abeaf18babebe77094bd25c7
jenisha987 <jensastha987@gmail.com>
2024-09-09 16:29:16 +0545

    fetching data for products

--- e-commerce/src/components/ProductCard.jsx
+++ e-commerce/src/components/ProductCard.jsx
@@ -3,26 +3,25 @@ import { IoStar } from "react-icons/io5";
 
 export default function ProductCard({ id, title, price, desc, img, rating }) {
     return (
-        <div className="font-bold lg:w-[250px] w-[180px] md:w-[220px] bg-white p-5 flex flex-col rounded-lg gap-2">
+        <div className="font-bold w-full max-w-xs bg-white p-4 flex flex-col rounded-lg gap-3 shadow-md">
             <img
-                className="lg:w-auto lg:h-[250px] h-[150px] md:h-[200px] hover:scale-110 cursor-grab 
-                transition-all duration-500 ease-in-out" 
+                className="w-full md:h-48 lg:h-56 object-cover hover:scale-105 transition-transform duration-500 ease-in-out cursor-pointer"
                 src={img}
-                alt="product" 
+                alt={title}
             />
-            <div className="text-sm flex justify-between">
-                <h2>{title}</h2>
+            <div className="text-sm flex justify-between items-center">
+                <h2 className="truncate w-2/3">{title}</h2>
                 <span className="text-primary">Rs.{price}</span>
             </div>
-            <p className="text-sm font-normal">{desc.slice(0, 50)}...</p>
-            <div className="flex justify-between">
-                <span className="flex justify-center items-center">
+            <p className="text-xs text-gray-600">{desc.slice(0, 50)}...</p>
+            <div className="flex justify-between items-center">
+                <span className="flex items-center">
                     <IoStar className="mr-1 text-yellow-400" /> {rating}
                 </span>
-                <button className="p-2 text-white bg-primary hover:bg-orange-700 rounded-lg text-sm">
+                <button className="p-1 md:p-2 text-white bg-primary hover:bg-orange-700 rounded-lg text-xs">
                     Add to Cart
                 </button>
             </div>
         </div>
-    )
+    );
 }
--- e-commerce/src/components/ProductList.jsx
+++ e-commerce/src/components/ProductList.jsx
@@ -4,23 +4,20 @@ import { products } from "../assets/products";
 
 export default function ProductList() {
     return (
-        <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-8">
-        {/* // <div className="flex flex-wrap gap-10 justify-center xl:justify-start my-10"> */}
-            {
-                products.map((product) => {
-                    return (
-                        <ProductCard 
-                            key={product.id} 
-                            id={product.id} 
-                            title={product.title} 
-                            price={product.price} 
-                            desc={product.description}
-                            rating={product.rating.rate}
-                            img={product.image}
-                        />
-                    )
-                })
-            }
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 py-6">
+            {products.map((product) => {
+                return (
+                    <ProductCard
+                        key={product.id}
+                        id={product.id}
+                        title={product.title}
+                        price={product.price}
+                        desc={product.description}
+                        // rating={product.rating.rate}
+                        img={product.images}
+                    />
+                );
+            })}
         </div>
-    )
+    );
 }
