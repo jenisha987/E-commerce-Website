c11a26eedb9f163c73d5c79294d33c027c8a1c32
jenisha987 <jensastha987@gmail.com>
2024-09-09 18:40:03 +0545

    Added cartSlice to store

--- e-commerce/src/redux/slices/CartSlice.jsx
+++ e-commerce/src/redux/slices/CartSlice.jsx
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const cartSlice = createSlice({
+export const CartSlice = createSlice({
     name: "cart",
     initialState: {
         cart: [],
--- e-commerce/src/redux/store.js
+++ e-commerce/src/redux/store.js
@@ -1,3 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import CartSlice from "./slices/CartSlice";
 
-export const store  = configureStore({});
+export const store  = configureStore({
+    reducer: {
+        cart: CartSlice,
+    }
+});
