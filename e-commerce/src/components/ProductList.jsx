e429034778ebd911f05ef4d214f2cce4ad51b11e
jenisha987 <jensastha987@gmail.com>
2024-09-09 13:33:22 +0545

    getting data from Fake Store Api

---
+++ e-commerce/src/components/ProductList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProductList() {
+    return (
+        <div>
+            <h3>Product List</h3>
+        </div>
+    )
+}
--- e-commerce/src/pages/Home.jsx
+++ e-commerce/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import ProductCategory from "../components/ProductCategory";
+import ProductList from "../components/ProductList";
 
 export default function Home() {
     return (
         <div className="container">
             <Header />
             <ProductCategory />
+            <ProductList />
         </div>
     )
 }
