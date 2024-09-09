0c8f5f3758e8a882d573971288ef1e6481dc9f9d
jenisha987 <jensastha987@gmail.com>
2024-09-09 12:52:03 +0545

    Scroll

--- e-commerce/src/components/ExploreMenu.jsx
+++ e-commerce/src/components/ExploreMenu.jsx
@@ -5,8 +5,14 @@ export default function ExploreMenu() {
     return (
         <div>
             <h3>Explore Our Products</h3>
-            <div>
+            <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden md:overflow-x-hidden">
                 <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">All</button>
+                <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">Men</button>
+                <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">Women</button>
+                <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">Kids</button>
+                <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">Men</button>
+                <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">Women</button>
+                <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white">Kids</button>
             </div>
         </div>
     )
