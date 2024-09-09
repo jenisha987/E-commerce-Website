4746e0f419c0efe5d66952d91df952f4f10e1db5
jenisha987 <jensastha987@gmail.com>
2024-09-09 14:22:06 +0545

    Styling card

--- e-commerce/src/components/ProductCard.jsx
+++ e-commerce/src/components/ProductCard.jsx
@@ -3,23 +3,25 @@ import { IoStar } from "react-icons/io5";
 
 export default function ProductCard() {
     return (
-        <div className="font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg">
+        <div className="font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg gap-2">
             <img
                 className="w-auto h-[250px] hover:scale-110 cursor-grab 
                 transition-all duration-500 ease-in-out" 
                 src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
                 alt="product" 
             />
-            <div>
+            <div className="text-sm flex justify-between">
                 <h2>Mens Bag</h2>
-                <span>Rs. 1800</span>
+                <span className="text-primary">Rs.1800</span>
             </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, quos?</p>
-            <div>
-                <span>
-                    <IoStar /> 4.5
+            <p className="text-sm font-normal ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, quos?</p>
+            <div className="flex justify-between">
+                <span className="flex justify-center items-center">
+                    <IoStar className="mr-1 text-yellow-400" /> 4.5
                 </span>
-                <button>Add to Cart</button>
+                <button className="p-2 text-white bg-primary hover:bg-orange-700 rounded-lg text-sm">
+                    Add to Cart
+                </button>
             </div>
         </div>
     )
