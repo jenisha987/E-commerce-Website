f7d7de4c40c28660d633cdb230de80352533a54b
jenisha987 <jensastha987@gmail.com>
2024-09-09 15:56:51 +0545

    Styling Products section

--- e-commerce/src/components/ProductCard.jsx
+++ e-commerce/src/components/ProductCard.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { IoStar } from "react-icons/io5";
 
-export default function ProductCard() {
+export default function ProductCard({ id, title, price, desc, img, rating }) {
     return (
-        <div className="font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg gap-2">
+        <div className="font-bold lg:w-[250px] w-[180px] md:w-[220px] bg-white p-5 flex flex-col rounded-lg gap-2">
             <img
-                className="w-auto h-[250px] hover:scale-110 cursor-grab 
+                className="lg:w-auto lg:h-[250px] h-[150px] md:h-[200px] hover:scale-110 cursor-grab 
                 transition-all duration-500 ease-in-out" 
-                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                src={img}
                 alt="product" 
             />
             <div className="text-sm flex justify-between">
-                <h2>Mens Bag</h2>
-                <span className="text-primary">Rs.1800</span>
+                <h2>{title}</h2>
+                <span className="text-primary">Rs.{price}</span>
             </div>
-            <p className="text-sm font-normal ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, quos?</p>
+            <p className="text-sm font-normal">{desc.slice(0, 50)}...</p>
             <div className="flex justify-between">
                 <span className="flex justify-center items-center">
-                    <IoStar className="mr-1 text-yellow-400" /> 4.5
+                    <IoStar className="mr-1 text-yellow-400" /> {rating}
                 </span>
                 <button className="p-2 text-white bg-primary hover:bg-orange-700 rounded-lg text-sm">
                     Add to Cart
--- e-commerce/src/components/ProductList.jsx
+++ e-commerce/src/components/ProductList.jsx
@@ -4,10 +4,21 @@ import { products } from "../assets/products";
 
 export default function ProductList() {
     return (
-        <div>
+        <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-8">
+        {/* // <div className="flex flex-wrap gap-10 justify-center xl:justify-start my-10"> */}
             {
                 products.map((product) => {
-                    return <ProductCard key={product.id} />
+                    return (
+                        <ProductCard 
+                            key={product.id} 
+                            id={product.id} 
+                            title={product.title} 
+                            price={product.price} 
+                            desc={product.description}
+                            rating={product.rating.rate}
+                            img={product.image}
+                        />
+                    )
                 })
             }
         </div>
