e2b6285767d4c9896c0cc0ca8c883774b95d75cc
jenisha987 <jensastha987@gmail.com>
2024-09-10 15:03:45 +0545

    added loading animation using react-spinners

--- e-commerce/src/pages/Success.jsx
+++ e-commerce/src/pages/Success.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { PropagateLoader } from "react-spinners";
 
 export default function Success() {
+    const [ loading, setLoading ] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000)
+    }, []);
+
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h2 className="text-3xl font-semibold mb-4">Order Successful!</h2>
-            <p>Your order has been successfully placed.</p>
+            {
+                loading ? (
+                    <PropagateLoader color="#EF8301" />
+                ) : (
+                    <div>
+                        <h2 className="text-3xl font-semibold mb-4 text-center">Order Successful!</h2>
+                        <p>Your order has been successfully placed.</p>
+                    </div>
+                )  
+            }
         </div>
     )
 }
