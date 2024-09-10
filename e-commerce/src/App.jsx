57a1f3474b173adce5c06d163ecb87838326df1e
jenisha987 <jensastha987@gmail.com>
2024-09-10 15:14:15 +0545

    added protected routes to fix empty cart

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -5,12 +5,13 @@ import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import Success from "./pages/Success";
 import Error from "./pages/Error";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout />} >
         <Route index element={<Home />} />
-        <Route path="/success" element={<Success />} />
+        <Route path="/success" element={<ProtectedRoute element={<Success />} />} />
         <Route path="/*" element={<Error />} />
     </Route>
 ))
