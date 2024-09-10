bc5ee2b5218f532d079516f27866367015fca7af
jenisha987 <jensastha987@gmail.com>
2024-09-10 14:02:21 +0545

    filtered data as per category

--- e-commerce/src/components/ProductList.jsx
+++ e-commerce/src/components/ProductList.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ProductCard from "./ProductCard";
 import { products } from "../assets/products";
 import toast, { Toaster } from 'react-hot-toast';
+import { useSelector } from "react-redux";
 
 export default function ProductList() {
+
     const handleToast = (title) => toast.success(`Added ${title}`);
+    const category = useSelector((state) => state.category.category);
+
     return (
         <>
             <Toaster
@@ -12,20 +16,34 @@ export default function ProductList() {
             reverseOrder={false}
             />
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 py-6">
-                {products.map((product) => {
-                    return (
+                {
+                    products.filter((product) => {
+                        if (category === "All") {
+                            return product;
+                        } else {
+                            return category === product.category.name;
+                        }
+                    }).map((product) => (
                         <ProductCard
-                        key={product.id}
-                        id={product.id}
-                        title={product.title}
-                        price={product.price}
-                        desc={product.description}
-                        // rating={product.rating.rate}
-                        img={product.images}
-                        handleToast={handleToast}
+                            key={product.id}
+                            id={product.id}
+                            title={product.title}
+                            price={product.price}
+                            desc={product.description}
+                            // rating={product.rating.rate}
+                            img={product.images}
+                            handleToast={handleToast}
                         />
+                    ))
+                }
+
+
+                {/* products.map((product) => {
+                    return (
+                        
                     );
-                })}
+                }) */}
+
             </div>
         </>
     );
